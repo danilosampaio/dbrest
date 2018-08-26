9bc418c2305af8b6802995f4fd340797e7fe70f8
Danilo Sampaio <danilo.sampaio@gmail.com>
2018-08-26 10:41:31 -0300

    Fix router in examples/dbrest-client

--- examples/dbrest-client/index.js
+++ examples/dbrest-client/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 const DBRest = require('dbrest');
 const http = require('http').Server(app);
 const bodyParser = require('body-parser');
@@ -8,13 +7,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const dbrest = new DBRest({
-    'dialect': 'mssql'
-});
-
 (async () => {
     try {
-        await dbrest.init(router);
+        const dbrest = new DBRest();
+        const router = await dbrest.init();
         app.use('/', router);
 
         http.listen(8081, function(){
